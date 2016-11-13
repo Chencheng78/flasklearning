507987d9373ce0a97c419c75c5ea7a0f4a642771
= <=>
2016-11-13 05:53:42 -0800

    chapter2

--- hello.py
+++ hello.py
@@ -1,8 +1,19 @@
-from flask import Flask,request
+from flask import Flask,request,make_response,redirect
+from flask_script import Manager
+
 app= Flask(__name__)
+manager = Manager(app)
+
 
 @app.route('/')
 def index():
+	'''
+	response = make_response('<h1>This is a cookie!</h1>')
+	response.set_cookie('answer','42')
+	return response
+	
+	return redirect('http://www.baidu.com')
+	'''
 	user_agent = request.headers.get('User-Agent')
 	return '<h1>Hello World!</h1><br><p>your browser is %s</p>' %user_agent
 
@@ -11,4 +22,4 @@ def user(name):
 	return '<h1>Hello %s!</h1>' %name
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	manager.run()
