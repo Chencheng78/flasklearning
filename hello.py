9a9e5bd517a39cf9eda0a17757ab5d0235c0bc41
geniuscc78 <geniuscc7@163.com>
2017-05-23 20:52:08 +0800

    chapter 3e

--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
 from flask import Flask, request, make_response, redirect, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 
 @app.route('/')
 def index():
@@ -16,7 +21,7 @@ def index():
 	'''
 	#user_agent = request.headers.get('User-Agent')
 	#return '<h1>Hello World!</h1><br><p>your browser is %s</p>' % user_agent
-	return render_template('index.html')
+	return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
