c50bddf7e9e41ef0acd7adbe9abbf2b41fef581c
geniuscc78 <geniuscc7@163.com>
2017-05-26 18:48:40 +0800

    chapter 4a web forms with flask-WTF.

--- hello.py
+++ hello.py
@@ -3,14 +3,19 @@ from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
 	'''
 	response = make_response('<h1>This is a cookie!</h1>')
@@ -21,7 +26,12 @@ def index():
 	'''
 	#user_agent = request.headers.get('User-Agent')
 	#return '<h1>Hello World!</h1><br><p>your browser is %s</p>' % user_agent
-	return render_template('index.html', current_time=datetime.utcnow())
+	name = None
+	form = NameFrom()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('index.html', current_time=datetime.utcnow(), form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
@@ -37,5 +47,12 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
 	return render_template('500.html'), 500
+
+
+class NameFrom(Form):
+	name = StringField('what\'s your name?', validators=[DataRequired()])
+	submit = SubmitField('Submit')
+
+
 if __name__ == '__main__':
 	manager.run()
