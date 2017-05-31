952806502f17b239920df37103d15c511188349a
geniuscc78 <geniuscc7@163.com>
2017-05-31 18:59:31 +0800

    chapter 5b

--- hello.py
+++ hello.py
@@ -58,15 +58,18 @@ def index():
 	# name = None
 	form = NameFrom()
 	if form.validate_on_submit():
-		# name = form.name.data
-		old_name = session.get('name')
-		if old_name is not None and old_name != form.name.data:
-			flash('Looks like you\'ve changed your name!')
+		user = User.query.filter_by(username=form.name.data).first()
+		if user is None:
+			user = User(username=form.name.data)
+			db.session.add(user)
+			session['known'] = False
+		else:
+			session['known'] = True
 		session['name'] = form.name.data
 		# form.name.data = ''
 		return redirect(url_for('index'))
 	return render_template('index.html', current_time=datetime.utcnow(),
-							form=form, name=session.get('name'), user_agent=user_agent)
+							form=form, name=session.get('name'), known=session.get('known', False), user_agent=user_agent)
 
 
 @app.route('/user/<name>')
